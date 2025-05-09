d119a631b3fdd303be06484f85ebdbbe081ffe24
meetsojitra08 <meetsojitra314@gmail.com>
2025-05-10 02:22:50 +0530

    Add files via upload

---
+++ tweet/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.tweet_list, name='tweet_list'),
+
+    path('create/', views.tweet_create, name='tweet_create'),
+
+    path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
+
+    path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
+
+    path('register/', views.register, name='register'),
+
+    path('login/', auth_views.LoginView.as_view(), name='login'), 
+
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('search/', views.search, name='search'), 
+    
+]
